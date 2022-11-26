0eac4823a56bb7c80852ee1aa2f4b4c82b56c969
shreyashdj <shreyashjadhav1799@gmail.com>
2022-11-26 09:55:38 +0530

    [Shreyash]: Added code to add Payroll Form

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import PayrollForm from './components/payroll form/Payroll Form.jsx';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
+import Header from './components/Header/Header.jsx';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Header />
+      <Router>
+        <Link to='/'></Link>
+        <Routes>
+          <Route path="/" element={<PayrollForm/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
