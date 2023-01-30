fb7505cea96d52919d55c6776197dfcde16fa589
Shreyash <shreyashjadhav1799@gmail.com>
2023-01-30 20:44:47 +0530

    [Shreyash]: Updated Code

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import { Switch } from 'react-router-dom'
 import Home from './components/Home/Home.jsx';
-import PayrollForm from './components/Payroll Form/Payroll Form.jsx';
+import PayrollForm from "./components/payroll form/Payroll Form.jsx"
+
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />}></Route>
           <Route path='/home' element={<Home />}></Route>
-          <Route exact path='/payrollform' element={<PayrollForm />}></Route>
+          <Route exact path='/payrollform' element={<PayrollForm/>}></Route>
           <Route exact path="/payrollform/:id" element={<PayrollForm />}></Route>
         </Routes>
       </Router>
